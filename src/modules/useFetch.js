a2a3434dd72540a16548093e510a1ffb8da9eb49
adnan <rizquadnan@gmail.com>
2022-02-13 14:38:03 +0700

    change base url

--- src/modules/useFetch.js
+++ src/modules/useFetch.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3001/api/v1/'
+const API_BASE_URL = 'https://madnanrizqullah-face-detector.herokuapp.com/api/v1/'
 
 function useFetch() {
   const defaultHeaders = {
