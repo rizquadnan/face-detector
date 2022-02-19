613ae7d47e6a38590cbc0e9141270b41c165042f
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-02-19 10:55:31 +0700

    debug base url

--- src/config/index.js
+++ src/config/index.js
@@ -3,4 +3,7 @@ const API_BASE_URL =
     ? 'https://madnanrizqullah-face-detector.herokuapp.com/api/v1/'
     : 'http://localhost:3001/api/v1/'
 
+console.log("API_BASE_URL", API_BASE_URL);
+console.log("process.env.NODE_ENV", process.env.NODE_ENV);
+
 export { API_BASE_URL };
