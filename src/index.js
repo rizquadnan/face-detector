463bb6e67ef2b19360d8985b0961314be33a9124
Muhammad Adnan R <madnanrizqullah@gmail.com>
2022-02-19 12:25:05 +0700

    remove tachyons

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App'
 import { TokenProvider } from './components/TokenProvider/TokenProvider'
 import { UserProvider } from './components/UserProvider/UserProvider'
-import 'tachyons'
 
 ReactDOM.render(
   <React.StrictMode>
