abc43e187dc035561b4908272f755374b42321a8
Muhammad Adnan R <madnanrizqullah@gmail.com>
2023-01-16 05:38:41 +0700

    fix: add default value to count

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,7 +1,7 @@
 const Rank = ({ nameOfUser, count }) => {
   return (
     <header style={{ textAlign: "center", fontSize: "1rem"}}>
-      <span style={{ display: "block"}}><span>{nameOfUser}</span> your face detect count is {count}</span>
+      <span style={{ display: "block"}}><span>{nameOfUser}</span> your face detect count is {count ?? 0}</span>
     </header>
   )
 };
