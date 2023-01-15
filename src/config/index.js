895d30594f41ffdcaf2b8379c91463d4a602ac80
Muhammad Adnan R <madnanrizqullah@gmail.com>
2023-01-16 05:38:26 +0700

    feat: change prod url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://madnanrizqullah-face-detector.herokuapp.com/api/v1/'
-    : 'http://localhost:3001/api/v1/'
+  process.env.NODE_ENV === "production"
+    ? "https://face-detector-api-production.up.railway.app/api/v1/"
+    : "http://localhost:3001/api/v1/";
 
 export { API_BASE_URL };
